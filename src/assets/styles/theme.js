6734039b003d0bddd491be36e7ba62edb1ea0d45
Paulina Słabosz <sejmipool@gmail.com>
2022-03-07 19:08:00 +0100

    installed styled-components, added GlobalStyle and theme for the projects, styled root component

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#111111',
+    darkBlue: '#4F74F0',
+    lightBlue: '#3D9AF1',
+    grey: '#E5E5E5',
+    darkGrey: '#C4C4C4',
+  },
+  fontSize: {
+    l: '30px',
+    m: '20px',
+    s: '16px',
+  },
+};
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,12 +1,26 @@
+import React from 'react';
 import './Root.css';
 import Card from '../components/Card';
+import styled, { ThemeProvider } from 'styled-components';
+import { theme } from '../assets/styles/theme';
+import { GlobalStyle } from '../assets/styles/GlobalStyle';
 
-function Root() {
-  return (
-    <div className="App">
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.grey};
+`;
+
+const Root = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Wrapper>
       <Card />
-    </div>
-  );
-}
+    </Wrapper>
+  </ThemeProvider>
+);
 
 export default Root;
