0857d885fe331842bd333ebe2dc990b414efd825
Paulina Słabosz <sejmipool@gmail.com>
2022-03-07 19:17:22 +0100

    basic styling for card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Personal from './Personal';
 import About from './About';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  max-width: 1070px;
+  width: 100%;
+  height: 650px;
+  background: linear-gradient(180deg, #3d9af1 0%, #4f74f0 100%);
+  border-radius: 40px;
+  display: flex;
+  box-shadow: 0px 0px 28px -17px rgba(66, 68, 90, 1);
+`;
+
 const Card = () => (
-  <div>
+  <Wrapper>
     <Personal />
     <About />
-  </div>
+  </Wrapper>
 );
 
 export default Card;
